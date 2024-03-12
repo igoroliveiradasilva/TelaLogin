7e5c244d72e1a45ed63fde8fcb28c3d092355df1
igor <igoroliveiras1909@gmail.com>
2024-03-12 00:36:11 -0300

    ajuste

--- getUsers.js
+++ getUsers.js
@@ -1,3 +1,9 @@
 fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(json => console.log(json))
+      {
+        "userId"; 1,
+        "id"; 1,
+        "title"; "delectus aut autem",
+        "completed"; false
+      }
