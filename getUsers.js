0ea4dab9f39671e4b7c5fa5d23b0847a1bc5af51
igor <igoroliveiras1909@gmail.com>
2024-03-12 00:57:59 -0300

    ajuste correto!

--- getUsers.js
+++ getUsers.js
@@ -1,4 +1,4 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(json => console.log(json))
       {
