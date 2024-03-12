6beb0b4e24f44b277bc3f784e943e5c4d27b0f0c
igor <igoroliveiras1909@gmail.com>
2024-03-12 00:19:14 -0300

    adicionado js

---
+++ index.js
@@ -0,0 +1,20 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Previne o envio do formulário
+
+        // Obter os valores dos campos de nome de usuário e senha
+        var nomeUsuario = document.getElementById('loginNome').value;
+        var senha = document.getElementById('loginSenha').value;
+
+        // Verificar as credenciais
+        if (nomeUsuario === 'usuario' && senha === 'senha') {
+            // Credenciais corretas, redirecionar ou exibir uma mensagem de sucesso
+            alert('Login bem-sucedido');
+        } else {
+            // Credenciais incorretas, exibir uma mensagem de erro
+            alert('Credenciais incorretas');
+        }
+    });
+});
